61987852bdeeb0f6346e47452889853595499d35
getlouied <derienzo_louis@yahoo.com>
2024-07-30 11:40:09 -0400

    Add initial basic mathitical functions

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+let numberOne;
+let numberTwo;
+let operator;
+
+
+
+
+
 /**********************************MATH FUNCTIONS *******************************/
+
+// Addition
+function addNumbers(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    return num1 + num2;
+}
+
+// Subctraction
+function substractNumbers(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    return num1 - num2;
+}
+
+
+// Mulitplication
+function multiplyNumbers(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    return num1 * num2;
+}
+
+
+
+// Division 
+function divideNumbers(num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    return num1 / num2
+}
